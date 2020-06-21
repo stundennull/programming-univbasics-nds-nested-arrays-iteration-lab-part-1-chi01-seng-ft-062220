e27121a2f75aa6f644a17dcac50ed5117f23948d
Kyle Walters <kyle.p.walters@gmail.com>
2020-06-21 02:01:44 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,6 +1,5 @@
 def find_even_values(src)
-  src = ( [ [10, 11], [99, 50, 3, 4], [23, 41] ] ) 
-  count = 0 
+    count = 0 
     while count < src.count do 
       inner_count = 0 
       while inner_count < src[count].count  
