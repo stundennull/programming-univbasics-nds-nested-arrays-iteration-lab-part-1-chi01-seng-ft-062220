ebc8ad351587f03925094c562deab1d538b4ef72
Kyle Walters <kyle.p.walters@gmail.com>
2020-06-21 02:07:47 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,8 +3,8 @@ def find_even_values(src)
     while row_index < src.count do 
       element_index = 0 
       while element_index < src[row_index].count do  
-        if src[count][inner_count] % 2 == 0 
-          p src[count][inner_count]
+        if src[row_index][element_index] % 2 == 0 
+          p src[row_index][element_index]
           element_index += 1
         end
         count += 1
